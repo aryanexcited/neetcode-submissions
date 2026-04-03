f1cdfcba71750f48aeebb27944f35ee25452381d
aryan <aryanwillstudy123456@gmail.com>
2026-04-03 23:47:43 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        s.upper()
+        scopy = [char.lower() for char in s if char.isalnum()]
+        right = len(scopy)-1
+        while left < right:
+            if scopy[left]!=scopy[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
