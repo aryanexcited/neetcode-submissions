7904092af4339e4db225da1e184f22dae9d8fc51
aryan <aryanwillstudy123456@gmail.com>
2026-08-21 20:13:58 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        st = []
+        n = len(temperatures)
+        res = [0] * n
+        for i in range(n):
+            while st and temperatures[st[-1]] < temperatures[i]:
+                pi = st.pop()
+                res[pi] = i - pi
+            st.append(i)
+        
+        return res
