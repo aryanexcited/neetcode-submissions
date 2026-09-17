8d0796d6c7f8f40822158e9a812f9e14be1f85b6
aryan <aryanwillstudy123456@gmail.com>
2026-09-17 20:32:23 +0530

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+
+        row = len(heights)
+        col = len(heights[0])
+
+        atlantic = deque()
+        pacific = deque()
+        for i in range(col):
+            pacific.append((0,i))
+            atlantic.append((row-1,i))
+
+        for i in range(row):
+            pacific.append((i,0))
+            atlantic.append((i,col-1))
+
+        def bfs(queue):
+            ans = set()
+            while queue:
+                x, y = queue.popleft()
+                ans.add((x,y))
+                for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < row and 0 <= ny < col and (nx,ny) not in ans and heights[nx][ny] >= heights[x][y]:
+                        ans.add((nx,ny))
+                        queue.append((nx,ny))
+                    
+            return ans
+        
+        pacific_set = bfs(pacific)
+        atlantic_set = bfs(atlantic)
+
+        return [[r,c] for r,c in pacific_set & atlantic_set]
