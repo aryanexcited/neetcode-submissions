dab3490483e751da446ad0b1a7e1d66c2fc765fc
aryan <aryanwillstudy123456@gmail.com>
2026-09-18 22:48:41 +0530

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        row = len(board)
+        col = len(board[0])
+
+        safe = deque()
+        for i in range(col):
+            if board[0][i] == "O":
+                safe.append((0,i))
+            if board[row-1][i] == "O":
+                safe.append((row-1,i))
+
+        for j in range(row):
+            if board[j][0] == "O":
+                safe.append((j,0))
+            if board[j][col-1] == "O":
+                safe.append((j,col-1))
+        
+        def bfs():
+            while safe:
+                x, y = safe.popleft()
+                board[x][y] = "#"
+                for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < row and 0 <= ny < col and board[nx][ny] == "O":
+                        safe.append((nx,ny))
+            
+            for i in range(row):
+                for j in range(col):
+                    if board[i][j] == "O":
+                        board[i][j] = "X"
+                    elif board[i][j] == "#":
+                        board[i][j] = "O"
+
+        return bfs()
